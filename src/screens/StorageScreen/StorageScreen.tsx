339fb3ea2acd31687a729e2692013b536b3f666c
Mateusz Matyska <mateusz.matyska_extern@bsdex.de>
2022-05-13 19:25:16 +0200

    fix storage

--- src/screens/StorageScreen/StorageScreen.tsx
+++ src/screens/StorageScreen/StorageScreen.tsx
@@ -1,21 +1,27 @@
 import React from 'react'
-import { SafeAreaView, View } from 'react-native'
+import { SafeAreaView, View, ToastAndroid } from 'react-native'
 import ButtonComponent from '../../components/button/ButtonComponent'
 import { StorageService } from '../../services/StorageService'
 import { styles } from './StorageScreen.styles'
 
 const StorageScreen: React.FC<any> = () => {
 
-    const storeData = () => {
+    const storeData = async () => {
         const data = [
             'Elliot Alderson', 'Darlene Alderson', 'Angela Moss', 'Tyrell Wellick'
         ]
-        StorageService.storeDataObject('people',data) 
+        await StorageService.storeDataObject('people',data) 
+    }
+
+    const readData = async () => {
+        const data = await StorageService.getDatabject('people')
+        ToastAndroid.show(JSON.stringify(data),10)
     }
 
     return <SafeAreaView style={styles.container}>
         <View>
-            <ButtonComponent value={'Add Records to data base'} onPress={() => {storeData}} />
+            <ButtonComponent value={'Add Records to data base'} onPress={() => {storeData()}} />
+            <ButtonComponent value={'Add Records to data base'} onPress={() => {readData()}} />
         </View>
     </SafeAreaView>
 }
